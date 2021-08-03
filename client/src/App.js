1a0fa7154e73a1f0eedf927ce2d8b3f0dfd56f16
imaspork <tyler.strong1@gmail.com>
2021-08-02 22:32:44 -0700

    changed file structure, mongo db connection, cron job, local file saving
    1. removed files and changed file structure for a simpler approach
    2. set up back end API payload handling to mongoDB
    3. set up cron job to run at 00:01:00 every day to update crypto values
    4. saved files locally for front end access

--- client/src/App.js
+++ client/src/App.js
@@ -17,61 +17,45 @@ import DisplayData from "./DisplayData";
 import SignUpCreateAccount from "./SignUpCreateAccount";
 import NumberConvert from "./NumberConvert";
 import BuyCoins from "./BuyCoins";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-	const [user, setUser] = useState("Tyler");
+	const [user, setUser] = useState("");
 	const [usdBal, setUsdBal] = useState(98630);
 
 	return (
 		<ChakraProvider theme={myNewTheme}>
-			<Box bg="primary">
-				<Flex h="100vh" bg="primary">
-					<Box h="6em" w="100%" bg="secondary">
-						<Flex h="100%" align="center" p="5">
-							<Heading size="2xl" pr="10">
-								CryptoTrader
-							</Heading>
-							<Link fontWeight="bold" p="3">
-								Features
-							</Link>
-							<Link fontWeight="bold" p="3">
-								Learn
-							</Link>
-							<Link fontWeight="bold" p="3">
-								About us
-							</Link>
-							<Spacer></Spacer>
-							<Box>
-								{user ? (
-									<Flex p="5">
-										Balance: ${NumberConvert(usdBal)} USD
-									</Flex>
-								) : (
-									<Box></Box>
-								)}
-							</Box>
-							<Box>
-								{user ? (
-									<Text
-										fontSize="xl"
-										fontWeight="bold"
-										p="3"
-										borderRadius="10"
-										border="1px"
-									>
-										{user}
-									</Text>
-								) : (
-									<Box>
-										<SignUpCreateAccount />
-									</Box>
-								)}
-							</Box>
-						</Flex>
-					</Box>
-				</Flex>
-				<BuyCoins />
-			</Box>
+			<Router>
+				<Box bg="primary">
+					<Flex h="100vh" bg="primary">
+						<Box h="6em" w="100%" bg="secondary">
+							<Flex h="100%" align="center" p="5">
+								<Heading size="2xl" pr="10">
+									CryptoTrader
+								</Heading>
+								<Link fontWeight="bold" p="3">
+									Features
+								</Link>
+								<Link fontWeight="bold" p="3">
+									Learn
+								</Link>
+								<Link fontWeight="bold" p="3">
+									About us
+								</Link>
+								<Link fontWeight="bold" p="3">
+									Buy Coin
+								</Link>
+								<Spacer></Spacer>
+
+								<Box>
+									<SignUpCreateAccount />
+								</Box>
+							</Flex>
+						</Box>
+					</Flex>
+					<BuyCoins />
+				</Box>
+			</Router>
 		</ChakraProvider>
 	);
 }
--- server.js
+++ server.js
@@ -1,32 +1,93 @@
-const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const axios = require("axios");
+const db = require("./config/keys").mongoURI;
+const mongoURI = require("./config/keys");
+const cron = require("node-cron");
+const express = require("express");
+const path = require("path");
+var fs = require("fs");
 
-const items = require("./routes/api/items");
+const filePath = path.join(__dirname, "/data/crypto.JSON");
+
+mongoose.connect(db, { useNewUrlParser: true }, function (err) {
+	if (err) throw err;
+
+	console.log("Successfully connected");
+});
+
+// api fetch
+
+function cryptoYeet() {
+	axios
+		.get("https://api.coinlore.net/api/tickers/?start=0&limit=100")
+		.then(function (response) {
+			onSuccess(response);
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
+
+	let cryptoSchema = mongoose.Schema({
+		id: Number,
+		symbol: String,
+		name: String,
+		nameid: String,
+		rank: Number,
+		price_usd: Number,
+		percent_change_24h: Number,
+		percent_change_1h: Number,
+		market_cap_usd: Number,
+		volume24: Number,
+		csupply: Number,
+		time: Number,
+		expireAt: {
+			type: Date,
+			default: Date.now,
+			index: { expires: "86400" },
+		},
+	});
+
+	const cryptos = mongoose.model("cryptos", cryptoSchema);
+
+	function onSuccess(response) {
+		let array = response.data.data;
+		console.log(array);
+		const writeCrypto = fs.createWriteStream(filePath);
+		writeCrypto.write("[");
+		for (let i = 0; i < array.length; i++) {
+			let newCrypto = new cryptos({
+				id: array[i].id,
+				symbol: array[i].symbol,
+				name: array[i].name,
+				nameid: array[i].nameid,
+				rank: array[i].rank,
+				price_usd: array[i].price_usd,
+				percent_change_24h: array[i].percent_change_24h,
+				market_cap_usd: array[i].market_cap_usd,
+				volume24: array[i].volume24,
+				csupply: array[i].csupply,
+				time: array[i].time,
+			});
+			writeCrypto.write(JSON.stringify(array[i]) + ",");
+			newCrypto.save(function (err, result) {
+				if (err) {
+					console.log(err);
+				} else {
+					console.log(result);
+				}
+			});
+		}
+		writeCrypto.write("]");
+		writeCrypto.close();
+	}
+}
+
+cron.schedule("1 0 * * *", () => {
+	cryptoYeet();
+	console.log("Crypto has been yeeted your majesty");
+});
 
 const app = express();
+app.listen(8080, null);
 
-// Bodyparser
-
-app.use(bodyParser.json());
-
-// Database config
-
-const db = require("./config/keys").mongoURI;
-
-// mongo connection
-
-mongoose
-	.connect(db)
-	.then(() => {
-		console.log("Mongo Database Connected");
-	})
-	.catch((err) => console.log(err));
-
-// use routes
-
-app.use("/api/items", items);
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server started on port ${port}`));
+cryptoYeet();
